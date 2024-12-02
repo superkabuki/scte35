492faef403016331eb5146d0fa2b98a534cd455b
Adrian of Doom <a@iodisco.com>
2024-12-01 20:24:45 -0500

    xml2cue

--- superkabuki/sxp.py
+++ superkabuki/sxp.py
@@ -7,7 +7,7 @@ home of the SuperXmlParser class
 from xml.sax.saxutils import escape, unescape
 from .xml import t2s, un_camel, un_xml, strip_ns, iter_attrs
 from .descriptors import descriptor_map,k_by_v
-from .segmentation import table20
+from .segmentation import table20,table22
 
 class SuperXmlParser:
 
@@ -90,6 +90,24 @@ class SuperXmlParser:
                 else:
                     return results
 
+    def mk_cmd(self,exemel):
+        targets = ["TimeSignal","SpliceInsert"]
+        for target in targets:
+            out = self.mk(exemel,target)
+            if out:
+                return out
+
+    def mk_dscptrs(self,exemel):
+        dlist =[]
+        targets = [ "AvailDescriptor",
+                   "DTMFDescriptor",
+                "SegmentationDescriptor",
+                "TimeDescriptor",]
+        for target in targets:
+            dscptrs= self.mk(exemel,target)
+            dlist.extend(dscptrs)
+        return dlist
+
     def gimme(self,tags,exemel):
         the_list =[]
         for tag in tags:
@@ -100,74 +118,112 @@ class SuperXmlParser:
                     the_list.extend(element_list)
         return the_list
 
+    def gimme_one(self,tag,exemel):
+        one = self.gimme([tag],exemel)
+        if one:
+            return one[0]
+
     def spliceinfosection(self,exemel):
-        tags = ["SpliceInfoSection"]
+        tag = "SpliceInfoSection"
 
-        out =self.gimme(tags,exemel)
+        out =self.gimme_one(tag,exemel)
         if out:
-            return out[0]
-
-    def command(self,exemel):
-        tags=["SpliceInsert","TimeSignal"]
-        cmd =  self.gimme(tags,exemel)[0]
-        if cmd:
-            splice_time = self.splicetime(cmd['sub'])
-            cmd['attrs'].update(splice_time)
-            break_duration=self.breakduration(cmd['sub'])
-            cmd["attrs"].update(break_duration)
-            return cmd
+            return out['attrs']
         return {}
 
+    def command(self,exemel):
+        ts = self.timesignal(exemel)
+        if ts:
+            return ts
+        si = self.spliceinsert(exemel)
+        if si:
+            return si
+        return {}
+
+    def timesignal(self,exemel):
+        ts=self.gimme_one("TimeSignal",exemel)
+        if ts:
+            splice_time = self.splicetime(ts['sub'])
+            ts['attrs'].update(splice_time)
+            setme= {'name':'Time Signal','command_type':6,}            
+            ts['attrs'].update(setme)
+            return ts['attrs']
+        return {}
+            
     def spliceinsert(self,exemel):
-        pass
+        si =  self.gimme_one("SpliceInsert",exemel)
+        if si:
+            setme={"name":"Splice Insert",
+                   "command_type": 5,
+                "event_id_compliance_flag":True,
+                "program_splice_flag": False,}
+            splice_time = self.splicetime(exemel)
+            if splice_time:
+                setme["program_splice_flag"]=True
+            si['attrs'].update(setme)
+            break_duration=self.breakduration(exemel)
+            si["attrs"].update(break_duration)
+            si["attrs"]['avails_expected'] = bool(si["attrs"]['avails_expected'])           
+            return si['attrs']
+        return {}
 
     def splicetime(self,exemel):
-        splicetime =self.gimme(['SpliceTime'], exemel)
+        splicetime =self.gimme_one('SpliceTime', exemel)
         if splicetime:
-            return {"pts_time":splicetime[0]['attrs']['pts_time'],
-                    "time_specified_flag": True}
+            return {"pts_time":splicetime['attrs']['pts_time'],
+                    "time_specified_flag": True,}
         return {}
 
     def breakduration(self,exemel):
-        break_duration= self.gimme(['BreakDuration'],exemel)
+        break_duration= self.gimme_one('BreakDuration',exemel)
         if break_duration:
-            return {"break_duration":break_duration[0]['attrs']['duration'],
-                    "auto_return": break_duration[0]['attrs']['auto_return'],
+            return {"break_duration":break_duration['attrs']['duration'],
+                    "auto_return": break_duration['attrs']['auto_return'],
                     "duration_flag":True,}
         return {}
 
     def segmentationdescriptor(self,dscptr):
-        if dscptr["tag"]=="SegmentationDescriptor":
-            setme={"segmentation_event_id_compliance_indicator": True,
-            "program_segmentation_flag":True,
-            "segmentation_duration_flag": False,
-            "delivery_not_restricted_flag": True,
-            "segmentation_event_id" :hex(dscptr['attrs']["segmentation_event_id"]),}
-           #if self.segmentation_type_id in table22:
-            #   self.segmentation_message = table22[self.segmentation_type_id]
-            dscptr['attrs'].update(setme)
-            dr = self.deliveryrestrictions(dscptr['sub'])
-            dscptr['attrs'].update(dr)
-            dscptr["upids"] =self.gimme(['SegmentationUpid'], dscptr["sub"])
-        return dscptr
+        setme={"name":"Segmentation Descriptor","segmentation_event_id_compliance_indicator": True,
+        "program_segmentation_flag":True,
+        "segmentation_duration_flag": False,
+        "delivery_not_restricted_flag": True,
+        "segmentation_event_id" :hex(dscptr['attrs']["segmentation_event_id"]),}
+        if dscptr['attrs']['segmentation_type_id'] in table22:
+            dscptr['attrs']["segmentation_message"] = table22[dscptr['attrs']['segmentation_type_id']]
+        dscptr['attrs'].update(setme)
+        dr = self.deliveryrestrictions(dscptr['sub'])
+        dscptr['attrs'].update(dr)
+        dscptr['attrs']["upids"] =self.gimme(['SegmentationUpid'], dscptr["sub"])
+        return dscptr['attrs']
 
     def descriptors(self,exemel):
-        tags = [
-        "AvailDescriptor",
-        "DTMFDescriptor",
-        "SegmentationDescriptor",
-        "TimeDescriptor",]
-
-        dlist= self.gimme(tags,exemel)
+        dmap = {#"AvailDescriptor":,
+                #"DTMFDescriptor":,
+                "SegmentationDescriptor":self.segmentationdescriptor,
+                #"TimeDescriptor":
+                }
+        out=[]
+        dlist= self.gimme(list(dmap.keys()),exemel)  
         for dscptr in dlist:
-            dscptr = self.segmentationdescriptor(dscptr)
-        return dlist
+            out.append(dmap[dscptr['tag']](dscptr))
+        return out
 
     def deliveryrestrictions(self,exemel):
-        dr = self.gimme(['DeliveryRestrictions'], exemel)
+        dr = self.gimme_one('DeliveryRestrictions', exemel)
         if dr:
             setme={"delivery_not_restricted_flag":False,
-                   "device_restrictions": table20[dr[0]['attrs']["device_restrictions"]],}
-            dr[0]['attrs'].update(setme)
-            return dr[0]['attrs']
+                   "device_restrictions": table20[dr['attrs']["device_restrictions"]],}
+            dr['attrs'].update(setme)
+            return dr['attrs']
         return {}
+
+    def xml2cue(self,exemel):
+        splice_info=self.spliceinfosection(exemel)
+        cmd = self.command(exemel)
+        dscptrs= self.descriptors(exemel)
+        return {"info_section": splice_info,
+                "command":cmd,
+                "descriptors":dscptrs,}
+                
+                
+        
