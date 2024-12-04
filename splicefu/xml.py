f3a7879d3d6fb91a450f1dbf12a991ae35c6bf23
Adrian of Doom <a@iodisco.com>
2024-12-04 16:46:30 -0500

    dropping XmlParser class

--- splicefu/xml.py
+++ splicefu/xml.py
@@ -1,7 +1,6 @@
 """
 xml.py  The Node class for converting to xml,
         The NameSpace Class.
-        The XmlParser class for parsing an xml string for SCTE-35 data.
         and several helper functions
 """
 
@@ -61,8 +60,7 @@ def iter_attrs(attrs):
     """
     conv = {un_camel(k): un_xml(v) for k, v in attrs.items()}
     pts_vars = ["pts_time", "pts_adjustment", "duration", "segmentation_duration"]
-    return  {k: (t2s(v) if k in pts_vars else v) for k, v in conv.items()}
-
+    return {k: (t2s(v) if k in pts_vars else v) for k, v in conv.items()}
 
 
 def val2xml(val):
@@ -184,8 +182,8 @@ class Node:
         attrs2nodes attributes to elements
         """
         for k, v in self.attrs.items():
-            self.add_child( Node(name=key2xml(k), value=val2xml(v)),slot=0 )
-        self.attrs={}
+            self.add_child(Node(name=key2xml(k), value=val2xml(v)), slot=0)
+        self.attrs = {}
 
     def mk_ans(self, attrs):
         """
@@ -243,7 +241,7 @@ class Node:
     def _rendrd_children(self, rendrd, ndent, name):
         for child in self.children:
             rendrd += self.mk(child)
-        return f"{rendrd}{ndent}</{name}>\n".replace(" >",">")
+        return f"{rendrd}{ndent}</{name}>\n".replace(" >", ">")
 
     def mk_name(self):
         """
@@ -275,7 +273,7 @@ class Node:
             child.namespace.all = self.namespace.all
             child.namespace.uri = ""
 
-    def rendr_all(self,ndent,name):
+    def rendr_all(self, ndent, name):
         """
         rendr_all renders the Node instance and it's children in xml.
         """
@@ -283,7 +281,7 @@ class Node:
         if self.value:
             return f"{rendrd}{self.value}</{name}>\n"
         rendrd = f"{rendrd}\n"
-        rendrd.replace(" >",">")
+        rendrd.replace(" >", ">")
         if self.children:
             return self._rendrd_children(rendrd, ndent, name)
         return rendrd.replace(">", "/>")
@@ -297,10 +295,10 @@ class Node:
         obj.children_namespaces()
         name = obj.mk_name()
         ndent = obj.get_indent()
-        #obj.attrs2nodes()
+        # obj.attrs2nodes()
         if isinstance(obj, Comment):
             return obj.mk(obj)
-        return obj.rendr_all(ndent,name)
+        return obj.rendr_all(ndent, name)
 
     def add_child(self, child, slot=None):
         """
@@ -355,133 +353,3 @@ class Comment(Node):
         obj = self.chk_obj(obj)
         obj.set_depth()
         return f"{obj.get_indent()}<!-- {obj.name} -->\n"
-
-
-class XmlParser:
-    """
-    XmlParser is for parsing
-    a SCTE-35 Cue from  xml.
-    """
-    
-    DESCRIPTORS = [
-        "AvailDescriptor",
-        "DTMFDescriptor",
-        "SegmentationDescriptor",
-        "TimeDescriptor",
-    ]
-
-    def __init__(self):
-        self.active = None
-        self.node_list = []
-        self.depth=0
-        self.parent=None
-        self.open=False
-
-    def chk_node_list(self, node):
-        """
-        chk_node_list is used to track open xml nodes
-        """
-        if self.active in self.node_list:
-            self.node_list.remove(self.active)
-            self.open = False
-        elif node[-2] != "/":
-            self.node_list.append(self.active)
-            self.depth +=1
-            self.open = True
-
-    def mk_value(self, value, gonzo):
-        """
-        mk_value, if the xml node has a value, write it to self.spare
-
-        <name>value</name>
-
-        """
-        if value:
-            gonzo[self.active][un_camel(self.active)] = unescape(value)
-        return gonzo
-
-    def mk_active(self, node):
-        """
-        mk_active sets self.active to the current node name.
-        """
-        self.active=None
-        name = node[1:].split(" ", 1)[0].split(">", 1)[0]
-        name = strip_ns(name)
-        self.active = name.replace("/", "").replace(">", "")
-
-
-    def _split_attrs(self, node):
-        node = node.replace("='", '="').replace("' ", '" ')
-        attrs = [x for x in node.split(" ") if "=" in x]
-        return attrs
-
-    def mk_attrs(self, node):
-        """
-        mk_attrs parses the current node for attributes
-        and stores them in self.spare[self.active]
-        """
-        if "!--" in node:
-            return False
-        attrs = self._split_attrs(node)
-        parsed = {
-            x.split('="')[0]: unescape(x.split('="')[1].split('"')[0]) for x in attrs
-        }
-        it = iter_attrs(parsed)
-        return it
-
-    def parse(self, exemel, descriptor_parse=False):
-        """
-        parse parses an xml string for a SCTE-35 Cue.
-        """
-        gonzo={}
-        if not descriptor_parse:
-            gonzo = {"descriptors": []}
-        data = exemel.replace("\n", "").strip()
-        while ">" in data:
-            self.mk_active(data)
-            data, gonzo = self._parse_nodes(data, gonzo, descriptor_parse)
-        return gonzo
-
-    def _parse_nodes(self, data, gonzo, descriptor_parse=False):
-        if self.active in self.DESCRIPTORS and not descriptor_parse:
-            data, gonzo = self._parse_descriptor(data, gonzo)
-        else:
-            data, gonzo = self._parse_most(data, gonzo)
-        return data, gonzo
-
-    def _parse_most(self, data, gonzo):
-        """
-        parse_most parse everything except descriptor nodes
-        """
-        ridx = data.index(">")
-        this_node = data[: ridx + 1]
-        self.chk_node_list(this_node)
-        attrs = self.mk_attrs(this_node)
-        if self.active not in gonzo:
-            if  self.active not in ['!--','']:
-                gonzo[self.active] = attrs
-        data = data[ridx + 1 :]
-        if "<" in data:
-            lidx = data.index("<")
-            value = data[:lidx].strip()
-            gonzo = self.mk_value(value, gonzo)
-            data = data[lidx:]
-        return data, gonzo
-
-    def _parse_descriptor(self, data, gonzo):
-        """
-        mk_descriptor slices off an entire
-        descriptor xml node from data to parse.
-        """
-        sub_data = ""
-        tag = data[1:].split(" ", 1)[0].split('>',1)[0]
-        try:
-            sub_data = data[: data.index(f"</{tag}>") + len(tag) + 1]
-        except:
-            sub_data = data[: data.index("/>") + 2]
-        data = data.replace(sub_data, "")
-        sub_stuff = self.parse(sub_data, descriptor_parse=True)
-        if 'SegmentationDescriptor' in sub_stuff and 'SegmentationUpid' in sub_stuff:
-            sub_stuff['SegmentationDescriptor']['SegmentationUpid'] = sub_stuff.pop('SegmentationUpid')
-        gonzo["descriptors"].append(sub_stuff)
-        return data, gonzo
