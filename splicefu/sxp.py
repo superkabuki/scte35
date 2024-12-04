ad3ae013348e5c8ba1c9d238e3682a5183fd64e9
Adrian of Doom <a@iodisco.com>
2024-12-03 22:54:12 -0500

    Update sxp.py

--- splicefu/sxp.py
+++ splicefu/sxp.py
@@ -6,9 +6,10 @@ home of the SuperXmlParser class
 
 from xml.sax.saxutils import escape, unescape
 from .xml import t2s, un_camel, un_xml, strip_ns, iter_attrs
-from .descriptors import descriptor_map,k_by_v
-from .segmentation import table20,table22
+from .descriptors import descriptor_map, k_by_v
+from .segmentation import table20, table22
 from .upids import upid_map
+
 
 class SuperXmlParser:
 
@@ -37,10 +38,10 @@ class SuperXmlParser:
         for an xml element
         """
         value = ""
-        attrs={}
-        if tag[0] != '/':
-            tag =strip_ns(tag)
-        o_data=sub_data
+        attrs = {}
+        if tag[0] != "/":
+            tag = strip_ns(tag)
+        o_data = sub_data
         if ">" in sub_data:
             ridx = sub_data.index(">")
             this_node = sub_data[: ridx + 1]
@@ -49,8 +50,13 @@ class SuperXmlParser:
             if "<" in sub_data:
                 lidx = sub_data.index("<")
                 value = sub_data[:lidx].strip()
-                sub_data = sub_data[lidx:].replace("\t","")
-            return  {"tag":tag,"attrs":attrs,"this":value,"xml":o_data,}
+                sub_data = sub_data[lidx:].replace("\t", "")
+            return {
+                "tag": tag,
+                "attrs": attrs,
+                "this": value,
+                "xml": o_data,
+            }
 
     def mk_tag(self, data):
         """
@@ -69,9 +75,9 @@ class SuperXmlParser:
             return data[: data.index(f"</{tag}>") + len(tag) + 2]
         except:
             try:
-                return data[:data.index(f"{tag}>") + len(tag)]
+                return data[: data.index(f"{tag}>") + len(tag)]
             except:
-                return data[:data.index("/>") +1 ]
+                return data[: data.index("/>") + 1]
 
     def mk(self, exemel, target="SpliceInfoSection"):
         """
@@ -83,39 +89,39 @@ class SuperXmlParser:
         while data:
             sub_data = None
             tag = self.mk_tag(data)
-            if target in tag:         # don't even consider namespace
+            if target in tag:  # don't even consider namespace
                 sub_data = self.mk_element(data, tag)
                 results.append(sub_data + ">")
                 data = data.replace(sub_data, "")
             else:
-                data= data.split(">", 1)[1]
+                data = data.split(">", 1)[1]
                 if "<" in data:
                     data = data[data.index("<") :]
                 else:
                     return results
 
-   
-
-    def mk_cmd(self,exemel):
-        targets = ["TimeSignal","SpliceInsert"]
+    def mk_cmd(self, exemel):
+        targets = ["TimeSignal", "SpliceInsert"]
         for target in targets:
-            out = self.mk(exemel,target)
+            out = self.mk(exemel, target)
             if out:
                 return out
 
-    def mk_dscptrs(self,exemel):
-        dlist =[]
-        targets = [ "AvailDescriptor",
-                   "DTMFDescriptor",
-                "SegmentationDescriptor",
-                "TimeDescriptor",]
+    def mk_dscptrs(self, exemel):
+        dlist = []
+        targets = [
+            "AvailDescriptor",
+            "DTMFDescriptor",
+            "SegmentationDescriptor",
+            "TimeDescriptor",
+        ]
         for target in targets:
-            dscptrs= self.mk(exemel,target)
+            dscptrs = self.mk(exemel, target)
             dlist.extend(dscptrs)
         return dlist
 
-    def gimme(self,tags,exemel):
-        the_list =[]
+    def gimme(self, tags, exemel):
+        the_list = []
         for tag in tags:
             results = self.mk(exemel, tag)
             if results:
@@ -124,20 +130,20 @@ class SuperXmlParser:
                     the_list.extend(element_list)
         return the_list
 
-    def gimme_one(self,tag,exemel):
-        one = self.gimme([tag],exemel)
+    def gimme_one(self, tag, exemel):
+        one = self.gimme([tag], exemel)
         if one:
             return one[0]
 
-    def spliceinfosection(self,exemel):
+    def spliceinfosection(self, exemel):
         tag = "SpliceInfoSection"
 
-        out =self.gimme_one(tag,exemel)
+        out = self.gimme_one(tag, exemel)
         if out:
-            return out['attrs']
+            return out["attrs"]
         return {}
 
-    def command(self,exemel):
+    def command(self, exemel):
         ts = self.timesignal(exemel)
         if ts:
             return ts
@@ -146,97 +152,113 @@ class SuperXmlParser:
             return si
         return {}
 
-    def timesignal(self,exemel):
-        ts=self.gimme_one("TimeSignal",exemel)
+    def timesignal(self, exemel):
+        ts = self.gimme_one("TimeSignal", exemel)
         if ts:
-            splice_time = self.splicetime(ts['xml'])
-            ts['attrs'].update(splice_time)
-            setme= {'name':'Time Signal','command_type':6,}            
-            ts['attrs'].update(setme)
-            return ts['attrs']
+            splice_time = self.splicetime(ts["xml"])
+            ts["attrs"].update(splice_time)
+            setme = {
+                "name": "Time Signal",
+                "command_type": 6,
+            }
+            ts["attrs"].update(setme)
+            return ts["attrs"]
         return {}
-            
-    def spliceinsert(self,exemel):
-        si =  self.gimme_one("SpliceInsert",exemel)
+
+    def spliceinsert(self, exemel):
+        si = self.gimme_one("SpliceInsert", exemel)
         if si:
-            setme={"name":"Splice Insert",
-                   "command_type": 5,
-                "event_id_compliance_flag":True,
-                "program_splice_flag": False,}
+            setme = {
+                "name": "Splice Insert",
+                "command_type": 5,
+                "event_id_compliance_flag": True,
+                "program_splice_flag": False,
+            }
             splice_time = self.splicetime(exemel)
             if splice_time:
-                setme["program_splice_flag"]=True
-            si['attrs'].update(setme)
-            break_duration=self.breakduration(exemel)
+                setme["program_splice_flag"] = True
+            si["attrs"].update(setme)
+            break_duration = self.breakduration(exemel)
             si["attrs"].update(break_duration)
-            si["attrs"]['avails_expected'] = bool(si["attrs"]['avails_expected'])           
-            return si['attrs']
+            si["attrs"]["avails_expected"] = bool(si["attrs"]["avails_expected"])
+            return si["attrs"]
         return {}
 
-    def splicetime(self,exemel):
-        splicetime =self.gimme_one('SpliceTime', exemel)
+    def splicetime(self, exemel):
+        splicetime = self.gimme_one("SpliceTime", exemel)
         if splicetime:
-            return {"pts_time":splicetime['attrs']['pts_time'],
-                    "time_specified_flag": True,}
+            return {
+                "pts_time": splicetime["attrs"]["pts_time"],
+                "time_specified_flag": True,
+            }
         return {}
 
-    def breakduration(self,exemel):
-        break_duration= self.gimme_one('BreakDuration',exemel)
+    def breakduration(self, exemel):
+        break_duration = self.gimme_one("BreakDuration", exemel)
         if break_duration:
-            return {"break_duration":break_duration['attrs']['duration'],
-                    "auto_return": break_duration['attrs']['auto_return'],
-                    "duration_flag":True,}
+            return {
+                "break_duration": break_duration["attrs"]["duration"],
+                "auto_return": break_duration["attrs"]["auto_return"],
+                "duration_flag": True,
+            }
         return {}
 
-    def segmentationdescriptor(self,dscptr):
-        setme={"name":"Segmentation Descriptor","segmentation_event_id_compliance_indicator": True,
-        "program_segmentation_flag":True,
-        "segmentation_duration_flag": False,
-        "delivery_not_restricted_flag": True,
-        "segmentation_event_id" :hex(dscptr['attrs']["segmentation_event_id"]),}
-        if dscptr['attrs']['segmentation_type_id'] in table22:
-            dscptr['attrs']["segmentation_message"] = table22[dscptr['attrs']['segmentation_type_id']]
-        dscptr['attrs'].update(setme)
-        dr = self.deliveryrestrictions(dscptr['xml'])
-        dscptr['attrs'].update(dr)
-        upids =self.gimme(['SegmentationUpid'], dscptr["xml"])
-        if len(upids)==1:
-            seg_upid_type= upids[0]['attrs']['segmentation_upid_type']
-            the_upid={"segmentation_upid":upids[0]['this'],
-                  'segmentation_upid_type':seg_upid_type,
-                  'segmentation_upid_type_name':upid_map[seg_upid_type][0]
-                  }
-        dscptr['attrs'].update(the_upid)
-        return dscptr['attrs']
+    def segmentationdescriptor(self, dscptr):
+        setme = {
+            "name": "Segmentation Descriptor",
+            "segmentation_event_id_compliance_indicator": True,
+            "program_segmentation_flag": True,
+            "segmentation_duration_flag": False,
+            "delivery_not_restricted_flag": True,
+            "segmentation_event_id": hex(dscptr["attrs"]["segmentation_event_id"]),
+        }
+        if dscptr["attrs"]["segmentation_type_id"] in table22:
+            dscptr["attrs"]["segmentation_message"] = table22[
+                dscptr["attrs"]["segmentation_type_id"]
+            ]
+        dscptr["attrs"].update(setme)
+        dr = self.deliveryrestrictions(dscptr["xml"])
+        dscptr["attrs"].update(dr)
+        upids = self.gimme(["SegmentationUpid"], dscptr["xml"])
+        if len(upids) == 1:
+            seg_upid_type = upids[0]["attrs"]["segmentation_upid_type"]
+            the_upid = {
+                "segmentation_upid": upids[0]["this"],
+                "segmentation_upid_type": seg_upid_type,
+                "segmentation_upid_type_name": upid_map[seg_upid_type][0],
+            }
+        dscptr["attrs"].update(the_upid)
+        return dscptr["attrs"]
 
-    def descriptors(self,exemel):
-        dmap = {#"AvailDescriptor":,
-                #"DTMFDescriptor":,
-                "SegmentationDescriptor":self.segmentationdescriptor,
-                #"TimeDescriptor":
-                }
-        out=[]
-        dlist= self.gimme(list(dmap.keys()),exemel)  
+    def descriptors(self, exemel):
+        dmap = {  # "AvailDescriptor":,
+            # "DTMFDescriptor":,
+            "SegmentationDescriptor": self.segmentationdescriptor,
+            # "TimeDescriptor":
+        }
+        out = []
+        dlist = self.gimme(list(dmap.keys()), exemel)
         for dscptr in dlist:
-            out.append(dmap[dscptr['tag']](dscptr))
+            out.append(dmap[dscptr["tag"]](dscptr))
         return out
 
-    def deliveryrestrictions(self,exemel):
-        dr = self.gimme_one('DeliveryRestrictions', exemel)
+    def deliveryrestrictions(self, exemel):
+        dr = self.gimme_one("DeliveryRestrictions", exemel)
         if dr:
-            setme={"delivery_not_restricted_flag":False,
-                   "device_restrictions": table20[dr['attrs']["device_restrictions"]],}
-            dr['attrs'].update(setme)
-            return dr['attrs']
+            setme = {
+                "delivery_not_restricted_flag": False,
+                "device_restrictions": table20[dr["attrs"]["device_restrictions"]],
+            }
+            dr["attrs"].update(setme)
+            return dr["attrs"]
         return {}
 
-    def xml2cue(self,exemel):
-        splice_info=self.spliceinfosection(exemel)
+    def xml2cue(self, exemel):
+        splice_info = self.spliceinfosection(exemel)
         cmd = self.command(exemel)
-        dscptrs= self.descriptors(exemel)
-        return {"info_section": splice_info,
-                "command":cmd,
-                "descriptors":dscptrs,}
-                
-                
-        
+        dscptrs = self.descriptors(exemel)
+        return {
+            "info_section": splice_info,
+            "command": cmd,
+            "descriptors": dscptrs,
+        }
