b4f3f52cebc9e8aabdc043a42c6c25017a0c6fe5
Adrian of Doom <a@iodisco.com>
2024-12-05 23:43:59 -0500

    black

--- splicefu/upids.py
+++ splicefu/upids.py
@@ -382,7 +382,11 @@ class Umid(Upid):
 # segmentation_upid_type : [name, class, length]
 upid_map = {
     0x00: ["No UPID", NoUpid, 0],
-    0x01: ["Type 0x01 is deprecated. Use type 0x0C, MPU.",Upid,False,],
+    0x01: [
+        "Type 0x01 is deprecated. Use type 0x0C, MPU.",
+        Upid,
+        False,
+    ],
     0x02: ["Type 0x02 is deprecated. Use type 0x03, AdID.", Upid, 8],
     0x03: ["AdID", Upid, 12],
     0x04: ["UMID", Umid, 32],
