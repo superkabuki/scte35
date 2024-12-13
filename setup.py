200ae3ebd0edd03b879fe555b09637f873d6ae31
Adrian of Doom <a@iodisco.com>
2024-12-13 01:59:28 -0500

    threefive3

--- setup.py
+++ setup.py
@@ -5,27 +5,25 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     readme = fh.read()
 
-with open("scte35/version.py","r", encoding="utf-8") as latest:
-    version = latest.read().split('"')[1]
+from threefive3.version import version
+
 
 setuptools.setup(
-    name="scte35",
+    name="threefive3",
     version=version,
     author="Adrian of Doom",
     author_email="spam@iodisco.com",
-    description="scte35 is a SCTE-35 Decoder and Encoder for MPEGTS, HLS and Everything else.",
+    description="threefive continued.",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/superkabuki/scte35",
     install_requires=[
-        'iframes >= 0.0.7',
         'm3ufu >= 0.0.89',
-        'new_reader >= 0.1.11',
         'sideways >= 0.0.23',
         "pyaes",
     ],
 
-    scripts=['bin/scte35'],
+    scripts=['bin/threefive3'],
     packages=setuptools.find_packages(),
     classifiers=[
         "License :: OSI Approved :: Sleepycat License",
@@ -34,10 +32,6 @@ setuptools.setup(
         "Operating System :: POSIX :: BSD :: OpenBSD",
         "Operating System :: POSIX :: BSD :: NetBSD",
         "Operating System :: POSIX :: Linux",
-        "Operating System :: POSIX :: SCO",
-        "Operating System :: Unix",
-        "Operating System :: Microsoft :: Windows :: Windows 3.1 or Earlier",
-        "Operating System :: Other OS",
         "Topic :: Multimedia :: Video",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: PyPy",
@@ -45,3 +39,4 @@ setuptools.setup(
     ],
     python_requires=">=3.6",
 )
+
