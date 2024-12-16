10f3805459973b6182018d6b2087794331767478
Adrian of Doom <a@iodisco.com>
2024-12-16 02:24:08 -0500

    switched to fu parser

--- threefive3/sxp.py
+++ threefive3/sxp.py
@@ -13,6 +13,14 @@ from .upids import upid_map
 
 class SuperXmlParser:
 
+    CHILD_NODES=[
+                    "Program",
+                    "SpliceTime",
+                    "DeliveryRestrictions",
+                    "SegmentationUpid",
+                    "BreakDuration",
+                ]
+    
     def _split_attrs(self, node):
         node = node.replace("='", '="').replace("' ", '" ')
         attrs = [x for x in node.split(" ") if "=" in x]
@@ -23,40 +31,16 @@ class SuperXmlParser:
         mk_attrs parses the current node for attributes
         and stores them in self.stuff[self.active]
         """
-        if "!--" in node:
-            return False
-        attrs = self._split_attrs(node)
-        parsed = {
-            x.split('="')[0]: unescape(x.split('="')[1].split('"')[0]) for x in attrs
-        }
-        it = iter_attrs(parsed)
-        return it
-
-    def parsed(self, tag, sub_data):
-        """
-        parsed returns tag, attrs and value
-        for an xml element
-        """
-        value = ""
         attrs = {}
-        if tag[0] != "/":
-            tag = strip_ns(tag)
-        o_data = sub_data
-        if ">" in sub_data:
-            ridx = sub_data.index(">")
-            this_node = sub_data[: ridx + 1]
-            attrs = self.mk_attrs(this_node)
-            sub_data = sub_data[ridx + 1 :]
-            if "<" in sub_data:
-                lidx = sub_data.index("<")
-                value = sub_data[:lidx].strip()
-                sub_data = sub_data[lidx:].replace("\t", "")
-            return {
-                "tag": tag,
-                "attrs": attrs,
-                "this": value,
-                "xml": o_data,
+        try:
+            attrs = self._split_attrs(node)
+            parsed = {
+                x.split('="')[0]: unescape(x.split('="')[1].split('"')[0])
+                for x in attrs
             }
+            attrs = iter_attrs(parsed)
+        finally:
+            return attrs
 
     def mk_tag(self, data):
         """
@@ -64,170 +48,137 @@ class SuperXmlParser:
         next available xml tag from data
         """
         tag = data[1:].split(" ", 1)[0].split(">", 1)[0]
-        return tag.strip()
+        return strip_ns(tag.strip())
 
-    def mk_element(self, data, tag):
-        """
-        mk_element slices out the tag element
-        inclusive: <tag > stuff</tag> and <tag attr="1"/>
-        """
-        try:
-            return data[: data.index(f"</{tag}>") + len(tag) + 2]
-        except:
-            return data[: data.index("/>") + 1]
+    def _vrfy_sp(self, sp):
+        if sp and sp[0] not in ["!", "/"]:
+            return True
+        return False
 
-    def mk(self, exemel, target="SpliceInfoSection"):
+    def _mk_value(self, sp):
+        return sp.split(">")[1].replace("\n", "").strip()
+
+    def _assemble(self, sp):
+        return {
+            "name": self.mk_tag(sp),
+            "attrs": self.mk_attrs(sp),
+            "this": self._mk_value(sp),
+            "children": [],
+        }
+
+    def fu(self, exemel):
         """
-        parse parses an xml string for a SCTE-35 Cue.
+        fu slice up exemel into data chunks.
         """
         results = []
-        sub_data = None
-        data = exemel.replace("\n", "").strip()
-        while data:
-            sub_data = None
-            tag = self.mk_tag(data)
-            if target in tag:  # don't even consider namespace
-                sub_data = self.mk_element(data, tag)
-                results.append(sub_data + ">")
-                data = data.replace(sub_data, "")
-            else:
-                data = data.split(">", 1)[1]
-                if "<" in data:
-                    data = data[data.index("<") :]
+        exemel = exemel.replace("\n", "").strip()
+        splitted = exemel.split("<")
+        for sp in splitted:
+            if self._vrfy_sp(sp):
+                x = self._assemble(sp)
+                if x["name"] in self.CHILD_NODES:
+                    results[-1]["children"].append(x)
                 else:
-                    return results
+                    results.append(x)
+        return results
 
-    def gimme(self, tags, exemel):
-        """
-        gimme get all instances of tag from exemel and return a list of them.
-        """
-        the_list = []
-        for tag in tags:
-            element_list = [self.parsed(tag, sd) for sd in self.mk(exemel, tag)]
-            if element_list:
-                the_list.extend(element_list)
-        return the_list
-
-    def gimme_one(self, tag, exemel):
-        """
-        gimme_one get the first instance of tag from exemel and return it.
-        """
-        one = self.gimme([tag], exemel)
-        if one:
-            return one[0]
-
-    def spliceinfosection(self, exemel):
+    def spliceinfosection(self, sis):
         """
         spliceinfosection parses exemel for info section data
         and returns a loadable dict
         """
-        my_name = "SpliceInfoSection"
+        return sis["attrs"]
 
-        out = self.gimme_one(my_name, exemel)
-        if out:
-            return out["attrs"]
-        return {}
-
-    def command(self, exemel):
+    def command(self, results):
         """
         command parses exemel for a splice command
         and returns a loadable dict
 
         """
-        ts = self.timesignal(exemel)
-        if ts:
-            return ts
-        si = self.spliceinsert(exemel)
-        if si:
-            return si
-        pc = self.privatecommand(exemel)
-        if pc:
-            return pc
+        cmap = {
+            "TimeSignal": self.timesignal,
+            "SpliceInsert": self.spliceinsert,
+            "PrivateCommand": self.privatecommand,
+        }
+
+        for result in results:
+            if result["name"] in cmap:
+                return cmap[result["name"]](result)
         return {}
 
-    def timesignal(self, exemel):
+    def timesignal(self, ts):
         """
         timesignal parses exemel for TimeSignal data
         and creates a loadable dict for the Cue class.
         """
-        my_name = "TimeSignal"
-        ts = self.gimme_one("TimeSignal", exemel)
-        if ts:
-            splice_time = self.splicetime(ts["xml"])
+        for child in ts["children"]:
+            splice_time = self.splicetime(child)
             ts["attrs"].update(splice_time)
+
             setme = {
                 "name": "Time Signal",
                 "command_type": 6,
             }
+
             ts["attrs"].update(setme)
             return ts["attrs"]
         return {}
 
-    def privatecommand(self, exemel):
+    def privatecommand(self, pc):
         """
         privatecommand parses exemel for PrivateCommand
         data and creates a loadable dict for the Cue class.
         """
-        my_name = "PrivateCommand"
-        pc = self.gimme_one("PrivateCommand", exemel)
-        if pc:
-            setme = {
-                "name": "Private Command",
-                "command_type": 255,
-                "private_bytes": pc["this"],
-            }
-            pc["attrs"].update(setme)
-            return pc["attrs"]
-        return {}
+        setme = {
+            "command_type": 255,
+            "private_bytes": pc["this"],
+        }
 
-    def spliceinsert(self, exemel):
+        pc["attrs"].update(setme)
+        return pc["attrs"]
+
+    def spliceinsert(self, si):
         """
         spliceinsert parses exemel for SpliceInsert data
         and creates a loadable dict for the Cue class.
         """
-        my_name = "SpliceInsert"
-        si = self.gimme_one(my_name, exemel)
-        if si:
-            setme = {
-                "name": my_name,
-                "command_type": 5,
-                "event_id_compliance_flag": True,
-                "program_splice_flag": False,
-                "duration_flag": False,
-            }
-            splice_time = self.splicetime(exemel)
-            if splice_time:
-                setme["program_splice_flag"] = True
-            si["attrs"].update(setme)
+        setme = {
+            "command_type": 5,
+            "event_id_compliance_flag": True,
+            "program_splice_flag": False,
+            "duration_flag": False,
+        }
+        si["attrs"].update(setme)
+        for child in si["children"]:
+            splice_time = self.splicetime(child)
             si["attrs"].update(splice_time)
-            break_duration = self.breakduration(exemel)
+            if "pts_time" in si["attrs"]:
+                si["attrs"]["program_splice_flag"] = True
+            break_duration = self.breakduration(child)
             si["attrs"].update(break_duration)
-            return si["attrs"]
-        return {}
+        return si["attrs"]
 
-    def splicetime(self, exemel):
+    def splicetime(self, st):
         """
         splicetime parses xml from a splice command
         to get pts_time, sets time_specified_flag to True
         """
-        splicetime = self.gimme_one("SpliceTime", exemel)
-        if splicetime:
+        if st["name"] == "SpliceTime":
             return {
-                "pts_time": splicetime["attrs"]["pts_time"],
+                "pts_time": st["attrs"]["pts_time"],
                 "time_specified_flag": True,
             }
         return {}
 
-    def breakduration(self, exemel):
+    def breakduration(self, bd):
         """
         breakduration parses xml for break duration, break_auto_return
         and sets duration_flag to True.
         """
-        break_duration = self.gimme_one("BreakDuration", exemel)
-        if break_duration:
+        if bd["name"] == "BreakDuration":
             return {
-                "break_duration": break_duration["attrs"]["duration"],
-                "break_auto_return": break_duration["attrs"]["auto_return"],
+                "break_duration": bd["attrs"]["duration"],
+                "break_auto_return": bd["attrs"]["auto_return"],
                 "duration_flag": True,
             }
         return {}
@@ -236,7 +187,6 @@ class SuperXmlParser:
         """
         segmentationdescriptor creates a dict to be loaded.
         """
-        my_name = "SegmentationDescriptor"
         setme = {
             "tag": 2,
             "identifier": "CUEI",
@@ -252,37 +202,36 @@ class SuperXmlParser:
                 dscptr["attrs"]["segmentation_type_id"]
             ]
         dscptr["attrs"].update(setme)
-        dr = self.deliveryrestrictions(dscptr["xml"])
-        dscptr["attrs"].update(dr)
-        the_upid = self.upids(dscptr["xml"])
-        dscptr["attrs"].update(the_upid)
+        for child in dscptr["children"]:
+            dr = self.deliveryrestrictions(child)
+            dscptr["attrs"].update(dr)
+            the_upid = self.upids(child)
+            dscptr["attrs"].update(the_upid)
         return dscptr["attrs"]
 
-    def upids(self, exemel):
+    def upids(self, upid):
         """
         upids parses out upids from a splice descriptors xml
         """
-        ulist = self.gimme(["SegmentationUpid"], exemel)
-        if len(ulist) == 1:
+        if upid["name"] == "SegmentationUpid":
             try:
-                seg_upid = bytes.fromhex(ulist[0]["this"].lower().replace("0x", ""))
+                seg_upid = bytes.fromhex(upid["this"].lower().replace("0x", ""))
             except ValueError:
-                seg_upid = ulist[0]["this"]
-            seg_upid_type = ulist[0]["attrs"]["segmentation_upid_type"]
+                seg_upid = upid["this"]
+            seg_upid_type = upid["attrs"]["segmentation_upid_type"]
             the_upid = {
-                "segmentation_upid": ulist[0]["this"],
+                "segmentation_upid": upid["this"],
                 "segmentation_upid_type": seg_upid_type,
                 "segmentation_upid_type_name": upid_map[seg_upid_type][0],
                 "segmentation_upid_length": len(seg_upid),
             }
             return the_upid
+        return {}
 
     def availdescriptor(self, dscptr):
-        my_name = "AvailDescriptor"
         setme = {
             "tag": 0,
             "identifier": "CUEI",
-            "name": my_name,
         }
         dscptr["attrs"].update(setme)
         return dscptr["attrs"]
@@ -299,7 +248,6 @@ class SuperXmlParser:
     ##        self.dtmf_count = len(self.dtmf_chars)
 
     def timedescriptor(self, dscptr):
-        my_name = "TimeDescriptor"
         setme = {
             "tag": 3,
             "identifier": "CUEI",
@@ -308,22 +256,21 @@ class SuperXmlParser:
         dscptr["attrs"].update(setme)
         return dscptr["attrs"]
 
-    def descriptors(self, exemel):
+    def descriptors(self, results):
         dmap = {
             "AvailDescriptor": self.availdescriptor,
             # "DTMFDescriptor",
             "SegmentationDescriptor": self.segmentationdescriptor,
-            # "TimeDescriptor",
+            "TimeDescriptor": self.timedescriptor,
         }
         out = []
-        dlist = self.gimme(list(dmap.keys()), exemel)
-        for dscptr in dlist:
-            out.append(dmap[dscptr["tag"]](dscptr))
+        for result in results:
+            if result["name"] in dmap:
+                out.append(dmap[result["name"]](result))
         return out
 
-    def deliveryrestrictions(self, exemel):
-        dr = self.gimme_one("DeliveryRestrictions", exemel)
-        if dr:
+    def deliveryrestrictions(self, dr):
+        if dr["name"] == "DeliveryRestrictions":
             setme = {
                 "delivery_not_restricted_flag": False,
                 "device_restrictions": table20[dr["attrs"]["device_restrictions"]],
@@ -337,12 +284,14 @@ class SuperXmlParser:
         xml2cue returns a base64 string for xmlbin
         and a dict for xml
         """
-        bindata = self.gimme_one("Binary", exemel)
-        if bindata:
-            return bindata["this"]
-        splice_info = self.spliceinfosection(exemel)
-        cmd = self.command(exemel)
-        dscptrs = self.descriptors(exemel)
+        results = self.fu(exemel)
+        for result in results:
+            if result["name"] == "Binary":
+                return result["this"]
+            if result["name"] == "SpliceInfoSection":
+                splice_info = self.spliceinfosection(result)
+        cmd = self.command(results)
+        dscptrs = self.descriptors(results)
         return {
             "info_section": splice_info,
             "command": cmd,
